a93b4a4cc84840b16c0373e5473bbd98ecc4ecb7
cristian-cuevas <cuevasgarciascristian@gmail.com>
2023-09-10 03:17:09 +0000

    repasando ando

---
+++ repaso.jsx
@@ -0,0 +1,28 @@
+// iniciliazar una variable con un valor
+var producto = "monitor de 24 pulgadas";
+console.log(producto)
+
+// las variables se puedes reasignar
+producto = "monitor de 19 pulgadas";
+console.log(producto)
+
+// JavaScript es un lenguaje de tipo dinamico, no se especifica tipo de dato 
+producto = 20;
+console.log(producto)
+
+// se puede inicilizae sin valor y asignar despues 
+var disponble;
+disponble = true;
+
+disponble = false;
+
+// inicilaizar multiples variables 
+var categoria = "computadora",
+    calificacion = "APLLE",
+    calificacion = 5;
+
+// las variables no pueden iniciar con numero
+
+//var 99dias;
+//var dias99;
+
